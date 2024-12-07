764ea823e8ea114b1ce217beb53aa1403cf9780e
YuvinMahwatta <yuvinmahawatta@gmail.com>
2024-12-07 22:17:41 +0530

    Defined the main folder and subfolder and created an empty dataframe

--- DSGP Data Cleaning/DiabeticsClean.py
+++ DSGP Data Cleaning/DiabeticsClean.py
@@ -1,3 +1,15 @@
 import pandas as pd
 import os
 import re
+
+
+root_folder = "D:\IIT\2 Year\Diabetics"
+categories = ['ALPHA GLUCO','SULFON','THIAZOL']
+
+combined_data = pd.DataFrame()
+
+
+
+
+
+
