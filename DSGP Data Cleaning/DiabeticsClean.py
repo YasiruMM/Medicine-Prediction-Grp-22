7b9161df8a356efb53bb20a3e0cd2d8b6869f073
YuvinMahwatta <yuvinmahawatta@gmail.com>
2024-12-08 00:19:31 +0530

    for loop to search the subfolders

--- DSGP Data Cleaning/DiabeticsClean.py
+++ DSGP Data Cleaning/DiabeticsClean.py
@@ -3,13 +3,21 @@ import os
 import re
 
 
-root_folder = "D:\IIT\2 Year\Diabetics"
+root_folder = r"D:\IIT\2 Year\Diabetics"
 categories = ['ALPHA GLUCO','SULFON','THIAZOL']
 
 combined_data = pd.DataFrame()
 
+def extract_dosage(Item_Name):
+
+    match = re.search(r'(\d+(\.\d+)?MG)', str(Item_Name))
+    if match:
+        return match.group(0)
+    return None
 
 
-
-
+# Process each medicine category (subfolder)
+for category in categories:
+    # Construct the path to the subfolder
+    category_folder = os.path.join(root_folder, category)
 
