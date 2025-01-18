bfe0d0d948cd1d0fc157741c7147347746ffccb8
Onaragamage <131785102+Onaragamage@users.noreply.github.com>
2025-01-18 19:22:16 +0530

    EDA_Cholesterol

--- Cholesterol_EDA.py
+++ Cholesterol_EDA.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+#balanna methna wena wenam gnna ekd hnd, nttn okkom ekt dl gnna ekd kyl
 drug1 = pd.read_csv("Cleaned_Simvastatin_Data.csv")
 drug2 = pd.read_csv("Rosuvastatin_Cleaned.csv")
 drug3 = pd.read_csv("Atorvastatin_Cleaned.csv")
