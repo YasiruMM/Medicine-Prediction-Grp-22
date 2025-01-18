2e2d03bc64fda9b6c74c86815b3c34ce1f6ed3bc
Onaragamage <131785102+Onaragamage@users.noreply.github.com>
2025-01-18 19:12:18 +0530

    EDA_Cholesterol

--- Cholesterol_EDA.py
+++ Cholesterol_EDA.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-# Replace with the actual file paths
 drug1 = pd.read_csv("Cleaned_Simvastatin_Data.csv")
 drug2 = pd.read_csv("Rosuvastatin_Cleaned.csv")
 drug3 = pd.read_csv("Atorvastatin_Cleaned.csv")
