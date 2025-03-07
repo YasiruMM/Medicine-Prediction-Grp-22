c63d0cbcd928e64ac401bf25ae6a97c2816111de
Akash Kavishka <akeekavizz@outlook.com>
2025-03-07 14:58:50 +0530

    Refactor Flask app to connect to Medicine_Accuracy_DB and add routes for cholesterol and diabetes data

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
+# filepath: /C:/Users/user/Documents/GitHub/Medicine-Prediction-Grp-22/app.py
 from flask import Flask, render_template
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
 # Connect to MongoDB
-client = MongoClient("mongodb://localhost:27017/")
-db = client["MedicineDB"]
-collection = db["Medicines"]
+client = MongoClient('mongodb://localhost:27017/')
+db = client['Medicine_Accuracy_DB']
 
-@app.route("/")
+@app.route('/')
 def index():
-    data = list(collection.find({}, {"_id": 0}))  # Fetch data, excluding _id
-    return render_template("index.html", data=data)
+    # Fetch data from MongoDB collection
+    collection = db['Cardiovascular']  # Ensure this is the correct collection name
+    data = list(collection.find())
+    return render_template('index.html', data=data)
 
-if __name__ == "__main__":
+@app.route('/cholesterol')
+def cholesterol():
+    # Fetch data from MongoDB collection
+    collection = db['Cholesterol']
+    data = list(collection.find())
+    return render_template('cholesterol.html', data=data)
+
+@app.route('/diabetes')
+def diabetes():
+    # Fetch data from MongoDB collection
+    collection = db['Diabetes']
+    data = list(collection.find())
+    return render_template('diabetes.html', data=data)
+
+if __name__ == '__main__':
     app.run(debug=True)
